4ca106a37e81eab846ce4cf444827240fca2081e
tarikjessoreit <tarikjessoreit@gmail.com>
2022-03-07 14:05:52 +0600

    update index page

--- index.php
+++ index.php
@@ -4,13 +4,56 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>PSR - Primary School Result</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
+	<div class="container-fluid m-0 p-0" id="header">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+		  <div class="container-fluid">
+		    <a class="navbar-brand" href="#">PSR</a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+		        <li class="nav-item">
+		          <a class="nav-link active" aria-current="page" href="#">Home</a>
+		        </li>
+
+		      </ul>
+		      <form class="d-flex">
+		        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+		        <button class="btn btn-danger" type="submit">Search</button>
+		      </form>
+		    </div>
+		  </div>
+		</nav>
+	</div>
+
+	<!-- main section -->
+	<div class="container">
+		<div class="row my-3 border-bottom">
+			<div class="col-md-12">
+				<h1>Dashboard</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, libero nam veniam! In quisquam voluptates incidunt alias, sapiente aliquid, sint ipsa dolore, dolores officia vel velit consectetur qui maiores, ab?
+			</div>
+		</div>
+	</div><!-- /main section -->
 
 
 
+
+	<div class="container-fluid m-0 p-0 mt-3" id="footer">
+		<div class="copy text-center border-top py-2">&copy;2022 by DEV-09  </div>
+	</div>
 <!-- JS links -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <script type="text/javascript" src="assets/main.js"></script>
 </body>
 </html>
