66b2c6726250ca313815e0574792d17ec23ae5c6
tarikjessoreit <tarikjessoreit@gmail.com>
2022-03-07 13:37:35 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PSR - Primary School Result</title>
+	<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+
+
+
+<!-- JS links -->
+<script type="text/javascript" src="assets/main.js"></script>
+</body>
+</html>
